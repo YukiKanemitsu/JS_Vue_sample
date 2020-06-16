d38367d2c3f47725402b4b28c0116fbae3ba37ee
YukiKanemitsu <y.kanemitsu0723@gmail.com>
2020-06-16 16:48:18 +0900

    [Lesson4]TodoList_途中

--- js/script.js
+++ js/script.js
@@ -1,198 +1,96 @@
-const todoList = []
-let inputForm, todoMain, tabButton, sortMenu
-let displayTarget = "inbox"
-let sortIndex = "created-desc"
-
-/** Todo一個単位のHTML文字列を生成する */
-function createTodoHtmlString(todo) {
-  let htmlString = ""
-  const editType = todo.isEdit ? "editFixed" : "edit"
-  const editButtonLabel = todo.isEdit ? "編集完了" : "編集"
-  const doneType = todo.isDone ? "inbox" : "done"
-  const doneButtonLabel = todo.isDone ? "未完了" : "完了"
-  let todoTextCell, priorityCell
-  if (todo.isEdit) {
-    todoTextCell =
-      '<td class="cell-text"><input class="input-edit" type="text" value=' +
-      todo.text +
-      " /></td>"
-    priorityCell =
-      '<td class="cell-priority"><input class="input-priority" type="number" value=' +
-      todo.priority +
-      ' /></td>'
-  } else {
-    todoTextCell = '<td class="cell-text">' + todo.text + "</td>"
-    priorityCell = '<td class="cell-priority">' + todo.priority + "</td>"
-  }
-  htmlString += '<tr id="' + todo.id + '">'
-  htmlString +=
-    '<td class="cell-edit-button"><button data-type="' +
-    editType +
-    '">' +
-    editButtonLabel +
-    '</button></td>'
-  htmlString += todoTextCell
-  htmlString += '<td class="cell-created-at">' + todo.createdAt + '</td>'
-  htmlString += priorityCell
-  htmlString += '<td class="cell-done">'
-  htmlString += '<button data-type="' + doneType + '">'
-  htmlString += doneButtonLabel
-  htmlString += '</button></td>'
-  htmlString += '</td>'
-  htmlString += '<td class="cell-delete">'
-  htmlString += '<button data-type="delete">'
-  htmlString += '削除'
-  htmlString += '</button></td>'
-  htmlString += '</tr>'
-  return htmlString
-}
-
-/** todoの完了ステートの変更 */
-function updateTodoState(todo, type) {
-  todo.isDone = type === "done"
-  updateTodoList()
-}
-
-/** ソート関数 */
-function sortTodos(a, b) {
-  switch (sortIndex) {
-    case "created-desc":
-      return Date.parse(b.createdAt) - Date.parse(a.createdAt)
-    case "created-asc":
-      return Date.parse(a.createdAt) - Date.parse(b.createdAt)
-    case "priority-desc":
-      return b.priority - a.priority
-    case "priority-asc":
-      return a.priority - b.priority
-    default:
-      return todoList
-  }
-}
-
-/** 編集モード */
-function editTodo(todo, type) {
-  todo.isEdit = type === "edit"
-  updateTodoList()
-}
-
-function deleteTodo(todo) {
-    
-    // 対象のTodoオブジェクトの配列内インデックスを調べる
-    const index = todoList.findIndex((t) => t.id === todo.id)
-
-    // 配列から削除する
-    todoList.splice(index, 1)
-}
-
-/** TodoListの描画を更新する */
-function updateTodoList() {
-  let htmlStrings = ""
-  // HTMLを書き換える
-  todoList
-    .filter(todo => todo.isDone !== (displayTarget === "inbox"))
-    .sort(sortTodos)
-    .forEach(todo => {
-      // 新しいHTMLを出力
-      htmlStrings += createTodoHtmlString(todo)
-      todoMain.innerHTML = htmlStrings
-    })
-  todoMain.innerHTML = htmlStrings
-  // 書き換えたHTMLにイベントをバインドする
-  todoList
-    .filter(todo => todo.isDone !== (displayTarget === "inbox"))
-    .forEach(todo => {
-      const todoEl = document.getElementById(todo.id)
-      if (todoEl) {
-        todoEl.querySelectorAll("button").forEach(btn => {
-          const type = btn.dataset.type
-          btn.addEventListener("click", event => {
-            if (type.indexOf("edit") >= 0) {
-              editTodo(todo, type)
-            } else if (type.indexOf('delete') >= 0) {
-                // 配列から指定のTodoオブジェクトを削除する
-                deleteTodo(todo)
-                updateTodoState(todo, type)
-            } else {
-                updateTodoState(todo, type)
-            }
-          })
-        })
-        // 編集状態の場合はテキストフィールドにもイベントをバインドする
-        if (todo.isEdit) {
-          todoEl.querySelector(".input-edit").addEventListener("input", event => {
-            todo.text = event.currentTarget.value
-          })
-          todoEl
-            .querySelector(".input-priority")
-            .addEventListener("input", event => {
-              todo.priority = parseInt(event.currentTarget.value, 10)
-            })
+// Vue アプリの生成
+function createApp() {
+  // 此処にVue.jsのコードを書く
+  new Vue({
+    el: '#wrapper',
+    data: {
+      filter: 'inbox',
+      text: '',
+      todos: [
+        {
+          id: 1, //識別用のID
+          text: 'みかんを買う',
+          createdAt: 1567940003455, // 登録日のUnixタイムスタンプ
+          done: false //タスクが完了したかどうか
+        },
+        {
+          id: 2,
+          text: '郵便物を出す',
+          createdAt: 1567940003455, // 登録日のUnixタイムスタンプ
+          done: false //タスクが完了したかどうか
+        },
+        {
+          id: 3, //識別用のID
+          text: 'バターを買う',
+          createdAt: 1567940003455, // 登録日のUnixタイムスタンプ
+          done: false //タスクが完了したかどうか
         }
+      ]
+    },
+    methods: {
+      formatDate: function(timestamp) {
+        
+        // 引数のtimestamp から Date オブジェクトの生成
+        const date = new Date(timestamp)
+
+        // date から年、月、日を取得
+        const year = date.getFullYear()
+        const month = date.getMonth() + 1
+        const day = date.getDate()
+
+        // 「年.月.日」の形式で日付を返す
+        return year + '.' + month + '.' + day
+      },
+      setFilter: function(filter) {
+        this.filter = filter
+      },
+      toggleTodo: function(id) {
+        this.todos = this.todos.map(function(todo) {
+          
+          // 引数の id を持つ todo の done を逆にする
+          if (todo.id === id) {
+            todo.done = !todo.done
+          }
+          return todo
+        })
+      },
+      handleSubmit: function() {
+
+        // テキストフィールドの内容をもとに Todo を追加する
+        this.addTodo(this.text)
+
+        // テキストフィールドを空にする
+        this.text = ''
+      },
+      addTodo: function(text) {
+        this.todos.push({
+          id: this.todosLength + 1,
+          text: text,
+          createdAt: Date.now(),
+          done: false          
+        })
       }
-    })
-}
-
-/** フォームをクリアする */
-function clearInputForm() {
-  inputForm["input-text"].value = ""
-}
-
-/** todoListを追加する */
-function addTodo(todoObj) {
-  todoObj.id = "todo-" + (todoList.length + 1)
-  todoObj.createdAt = new Date().toLocaleString()
-  todoObj.priority = 3
-  todoObj.isDone = false
-  todoObj.isEdit = false
-  todoList.unshift(todoObj)
-  updateTodoList()
-  clearInputForm()
-}
-
-/** Todoを登録する処理 */
-function handleSubmit(event) {
-  event.preventDefault()
-  const todoObj = {
-    text: inputForm["input-text"].value
-  }
-  addTodo(todoObj)
-}
-
-/** インボックス / 完了済の切り分け */
-function handleTabClick(event) {
-  const me = event.currentTarget
-  displayTarget = me.dataset.target
-  updateTodoList()
-}
-
-/** ソートの実行 */
-function handleSort(e) {
-  sortIndex = e.currentTarget.value
-  updateTodoList()
-}
-
-/** DOMを変数に登録する */
-function registerDOM() {
-  inputForm = document.querySelector("#input-form")
-  todoMain = document.querySelector("#todo-main")
-  tabButton = document.querySelector("#tab").querySelectorAll("button")
-  sortMenu = document.querySelector("#sort-menu")
-}
-
-/** DOMにイベントを設定する */
-function bindEvents() {
-  inputForm.addEventListener("submit", event => handleSubmit(event))
-  tabButton.forEach(tab => {
-    tab.addEventListener("click", event => handleTabClick(event))
+    },
+    computed: {
+      todosLength: function () {
+        return this.todos.length
+      },
+      filteredTodos: function() {
+        const filter = this.filter
+        return this.todos.filter(function(todo) {
+          return filter === 'completed' ? todo.done : !todo.done
+        })
+      },
+      disabled: function() {
+        return this.text === ''
+      },
+    }
   })
-  sortMenu.addEventListener("change", event => handleSort(event))
 }
 
-/** 初期化 */
+// 初期化
 function initialize() {
-  registerDOM()
-  bindEvents()
-  updateTodoList()
+  createApp()
 }
 
-document.addEventListener("DOMContentLoaded", initialize.bind(this))
+document.addEventListener('DOMContentLoaded', initialize.bind(this))
